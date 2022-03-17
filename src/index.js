46f14e8fd649b3dad56f5856fc8c5434bf8ad3de
Emilio Arcioni <emi@waytoodigital.com>
2022-03-17 19:51:35 -0300

    Cleanup and initial working commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,102 @@
-import * as React from 'react'
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
+let context = createContext();
+let { Provider } = context;
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
+export const useDiscord = () => useContext(context);
+
+export const DiscordProvider = ({ redirectUri, discordClientId, discordClientSecret, children }) => {
+
+  const [discordUser, setDiscordUser] = useState(null);
+  const [oauthData, setOauthData] = useState(null);
+  const [loadingDiscordUserData, setLoadingDiscordUserData] = useState(false);
+  const router = useRouter();
+  const code = router.query.code;
+
+  const getUserFromDiscord = useCallback(async (token_type, access_token) => {
+    try {
+      setLoadingDiscordUserData(true);
+      const userResult = await axios.get("https://discord.com/api/users/@me", {
+        headers: {
+          Authorization: `${token_type} ${access_token}`,
+        },
+      });
+      setLoadingDiscordUserData(false);
+      setDiscordUser(userResult.data);
+      return userResult.data;
+    } catch (e) {
+      setLoadingDiscordUserData(false);
+      throw e;
     }
-  }, [])
+  }, []);
 
-  return counter
+  const clearUserState = () => {
+    setDiscordUser(null);
+  };
+
+  const getTokenFromDiscord = useCallback(async () => {
+    try {
+      const oauthResult = await axios.post(
+        "https://discord.com/api/oauth2/token",
+        new URLSearchParams({
+          client_id: discordClientId,
+          client_secret: discordClientSecret,
+          code: code,
+          grant_type: "authorization_code",
+          redirect_uri: redirectUri,
+          scope: "identify",
+        })
+      );
+
+      setOauthData(oauthResult.data);
+      return oauthResult.data;
+    } catch (e) {
+      throw e;
+    }
+  }, [code]);
+
+  const loginWithDiscord = () => {
+    router.push(
+      `https://discord.com/api/oauth2/authorize?${new URLSearchParams({
+        client_id: discordClientId,
+        redirect_uri: redirectUri,
+        response_type: "code",
+        scope: "identify email guilds.join",
+      }).toString()}`
+    );
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const oauthData = await getTokenFromDiscord();
+        await getUserFromDiscord(oauthData.token_type, oauthData.access_token);
+      } catch (err) {}
+    };
+
+    if (code) getData();
+  }, [code, router, getTokenFromDiscord, getUserFromDiscord]);
+
+  return (
+    <Provider
+      value={{
+        loginWithDiscord,
+        discordUser,
+        clearUserState,
+        oauthData,
+        loadingDiscordUserData,
+        getUserFromDiscord,
+      }}
+    >
+      {children}
+    </Provider>
+  );
 }
